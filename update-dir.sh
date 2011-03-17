7e4a078babf1c33c0df1397def1f58cbbe19402c
Sumit Raja <sumit.raja@raja-consulting.co.uk>
2011-03-17 20:59:57 +0000

    Added p tags around anchors

--- update-dir.sh
+++ update-dir.sh
@@ -3,7 +3,7 @@
 for DIR in $(find ./maven-repo -type d); do
   (
     echo "<html><body><h1>Directory listing</h1><hr/><pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>",$1,$1 }'
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<p><a href=\"%s\">%s</a></p>",$1,$1 }'
     echo "</pre></body></html>"
   ) > "${DIR}/index.html"
 done
